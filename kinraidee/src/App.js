3999b55bdad04e8e5905315ebd8e0536ab91275a
supakrit-f <supakrit.fill@gmail.com>
2022-07-05 17:18:34 +0700

    .

--- kinraidee/src/App.js
+++ kinraidee/src/App.js
@@ -1,9 +1,24 @@
+import { React, useState } from 'react';
 import './App.css';
+import DataFood from './data/dataF.json';
 
 function App() {
+  const [result, setResult] = useState([])
+  const handleClick = () =>{
+    setResult(DataFood[Math.floor(Math.random() * DataFood.length)]);
+  }
   return (
     <div className="App">
-      
+      <div className="header">
+        <h1>KINRAIDEE</h1>
+      </div>
+      <div className="container">
+        <img src={result.image} />
+        <h2>{result.title}</h2>
+      </div>
+      <div className="box-button">
+        <button onClick={handleClick}>RANDOM</button>
+      </div>
     </div>
   );
 }
